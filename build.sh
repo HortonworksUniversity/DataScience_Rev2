e40bb9cc898a757971baff05822027957c6cb7dd
rfraposa <rich@hortonworks.com>
2014-06-25 19:28:00 -0600

    Added command-line argument named rebuild

--- build.sh
+++ build.sh
@@ -26,54 +26,40 @@ git pull
 REBUILD=false
 if [[ ($var2 == "rebuild") || ($var3 == "rebuild") ]]; then
 	$REBUILD=true;
-	echo -e "NOTE: Rebuilding Data Science Docker images..."
 fi
 
 if [[ $REBUILD == true  ]];
 then
+	echo -e "NOTE: Rebuilding Data Science Docker images..."
 
-# Build the Docker images
+	# Build the Docker images
+	# Build hwxu/hdp_python_node
+	echo -e "\n*** Building hwxu/hdp_python_node ***\n"
+	cd /root/$REPO_DIR/dockerfiles/hdp_python_node
+	docker build -t hwxu/hdp_python_node .
+	echo -e "\n*** Build of hwxu/hdp_python_node complete! ***\n"
 
-# Build hwxu/hdp_node_base first
-cd /root/dockerfiles/hdp_node_base
-x=$(docker images | grep -c  hwxu/hdp_node_base)
-if [ $x == 0 ]; then
-        echo -e "\n*** Building hwxu/hdp_node_base image... ***\n"
-        docker build -t hwxu/hdp_node_base .
-        echo -e "Build of hwxu/hdp_node_base complete!"
-else
-        echo -e "\n*** hwxu/hdp_node_base image already built ***\n"
-fi
+	# Build hwxu/hdp_mahout_node
+	echo -e "\n*** Building hwxu/hdp_mahout_node ***\n"
+	cd /root/$REPO_DIR/dockerfiles/hdp_mahout_node
+	docker build -t hwxu/hdp_mahout_node .
+	echo -e "\n*** Build of hwxu/hdp_mahout_node complete! ***\n"
 
+	# Build hwxu/hdp_spark_node
+	echo -e "\n*** Building hwxu/hdp_spark_node ***\n"
+	cd /root/$REPO_DIR/dockerfiles/hdp_spark_node
+	docker build -t hwxu/hdp_spark_node .
+	echo -e "\n*** Build of hwxu/hdp_spark_node complete! ***\n"
 
-# Build hwxu/hdp_python_node
-echo -e "\n*** Building hwxu/hdp_python_node ***\n"
-cd /root/$REPO_DIR/dockerfiles/hdp_python_node
-docker build -t hwxu/hdp_python_node .
-echo -e "\n*** Build of hwxu/hdp_python_node complete! ***\n"
+	# Build hwxu/ipython_node
+	echo -e "\n*** Building hwxu/ipython_node ***\n"
+	cd /root/$REPO_DIR/dockerfiles/ipython_node
+	docker build -t hwxu/ipython_node .
+	echo -e "\n*** Build of hwxu/ipython_node complete! ***\n"
 
-# Build hwxu/hdp_mahout_node
-echo -e "\n*** Building hwxu/hdp_mahout_node ***\n"
-cd /root/$REPO_DIR/dockerfiles/hdp_mahout_node
-docker build -t hwxu/hdp_mahout_node .
-echo -e "\n*** Build of hwxu/hdp_mahout_node complete! ***\n"
-
-# Build hwxu/hdp_spark_node
-echo -e "\n*** Building hwxu/hdp_spark_node ***\n"
-cd /root/$REPO_DIR/dockerfiles/hdp_spark_node
-docker build -t hwxu/hdp_spark_node .
-echo -e "\n*** Build of hwxu/hdp_spark_node complete! ***\n"
-
-# Build hwxu/ipython_node
-echo -e "\n*** Building hwxu/ipython_node ***\n"
-cd /root/$REPO_DIR/dockerfiles/ipython_node
-docker build -t hwxu/ipython_node .
-echo -e "\n*** Build of hwxu/ipython_node complete! ***\n"
-
-#If this script is execute multiple times, untagged images get left behind
-#This command removes any untagged Docker images
-docker rmi -f $(docker images | grep '^<none>' | awk '{print $3}')
-
+	#If this script is execute multiple times, untagged images get left behind
+	#This command removes any untagged Docker images
+	docker rmi -f $(docker images | grep '^<none>' | awk '{print $3}')
 fi
 
 # Add/modify DS root directory in start scripts 
