c34f76b4308466cd0de375d9d25bc6284ad13366
rfraposa <rich@hortonworks.com>
2014-06-26 09:24:41 -0600

    Added mkdir /root/notebooks

--- dockerfiles/ipython_node/scripts/startup.sh
+++ dockerfiles/ipython_node/scripts/startup.sh
@@ -3,4 +3,4 @@
 /etc/init.d/sshd start
 export HOME=/root
 cd /root/notebooks
-ipython notebook --profile=nbserver > /tmp/ipython.log 2>&1
+#ipython notebook --profile=nbserver > /tmp/ipython.log 2>&1
