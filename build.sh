c94d0365e733ea33c0ffe7be1716f93ef7a312c5
rfraposa <rich@hortonworks.com>
2014-06-25 19:07:09 -0600

    Added a new command-line option named rebuild

--- build.sh
+++ build.sh
@@ -19,7 +19,7 @@ git pull
 
 # By default, the following Docker images are not rebuilt. 
 # To rebuild these images, add a command-line argument named "rebuild"
-REBUILD=false
+$REBUILD=false
 # Skip the building of images by default
 if [[ ($2 == "rebuild") || ($3 == "rebuild") ]]; then
 	$REBUILD=true;
