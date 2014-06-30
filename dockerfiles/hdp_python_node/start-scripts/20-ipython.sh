91a09d6e1cedd427289367c2477fb35c73f69064
rfraposa <rich@hortonworks.com>
2014-06-29 23:19:37 -0600

    ds_cluster.sh now starts up a Spark cluster

--- dockerfiles/hdp_python_node/start-scripts/20-ipython.sh
+++ dockerfiles/hdp_python_node/start-scripts/20-ipython.sh
@@ -8,4 +8,5 @@ mkdir /root/nltk_data
 
 export HOME=/root
 cd /root/notebooks
-ipython notebook --profile=nbserver > /tmp/ipython.log 2>&1
+#ipython notebook --profile=nbserver > /tmp/ipython.log 2>&1
+IPYTHON_OPTS="notebook --pylab inline --profile nbserver" /usr/lib/spark/bin/pyspark
--- scripts/ds_cluster.sh
+++ scripts/ds_cluster.sh
@@ -28,28 +28,24 @@ CID_hive=$(docker run -d --privileged --link namenode:namenode -e namenode_ip=$I
 IP_hive=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" hiveserver)
 echo "Hive/Oozie running on $IP_hive"
 
-#Start the Mahout lab worker node
-echo "Starting node1 with Mahout lab content..."
-CID_mahout=$(docker run -v $DS_DIR/labs/Lab3.1:/root/labs/lab3.1:rw -d --privileged --link namenode:namenode -e NODE_TYPE=workernode -e namenode_ip=$IP_namenode --dns 8.8.8.8 -p 4567:4567 -p 22  --name node1 -h node1 -i -t hwxu/hdp_mahout_node bash)
-IP_mahout=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" node1)
-echo "Started node1 (with Mahout lab content) on $IP_mahout"
-
 #Start the IPython node
-echo "Starting node2 with the IPython server..."
-CID_ipython=$(docker run -d -v /root/notebooks:/root/notebooks -v /root/nltk_data:/root/nltk_data --privileged --link namenode:namenode -e NODE_TYPE=workernode -e namenode_ip=$IP_namenode --dns 8.8.8.8 -p 8888:8888 -p 22 --name node2 -h node2 -i -t hwxu/hdp_python_node bash)
-IP_ipython=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" node2)
+echo "Starting node1 with the IPython server..."
+CID_ipython=$(docker run -d -v /root/notebooks:/root/notebooks -v /root/nltk_data:/root/nltk_data --privileged --link namenode:namenode -e NODE_TYPE=workernode -e namenode_ip=$IP_namenode --dns 8.8.8.8 -p 8888:8888 -p 22 --name node1 -h node1 -i -t hwxu/hdp_spark_node bash)
+IP_ipython=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" node1)
 IP=$(/sbin/ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
 echo "IPython Notebook Server Started at http://$IP:8888/ (ssh IP is $IP_ipython)"
 
+
 #Start the WorkerNodes
-echo "Starting $((num_of_nodes-2)) additional WorkerNodes..."
-for (( i=3; i<=$num_of_nodes; ++i));
+echo "Starting $num_of_nodes WorkerNodes..."
+for (( i=2; i<=$num_of_nodes; ++i));
 do
 nodename="node$i"
-CID=$(docker run -d --privileged --link namenode:namenode -e namenode_ip=$IP_namenode -e NODE_TYPE=workernode --dns 8.8.8.8 -p 8010 -p 50075 -p 50010 -p 50020 -p 45454 -p 8081 -p 22 --name $nodename -h $nodename -i -t hwxu/hdp_python_node bash)
+CID=$(docker run -v $DS_DIR/labs/demos:/root/labs/demos:rw -d --privileged --link namenode:namenode -e namenode_ip=$IP_namenode -e NODE_TYPE=workernode --dns 8.8.8.8 -p 8010 -p 50075 -p 50010 -p 50020 -p 45454 -p 8081 -p 22 --name $nodename -h $nodename -i -t hwxu/hdp_spark_node)
 IP_workernode=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" $nodename)
 echo "Started $nodename on IP $IP_workernode"
 done
 
 #All the Containers are started
 echo "Cluster is up and running!"
+
