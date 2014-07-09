e611596b1ecf5cbfd733005e0bb9ccdeb426116c
rfraposa <rich@hortonworks.com>
2014-07-09 08:08:49 -0600

    No longer using pyspark for IPython

--- dockerfiles/hdp_python_node/start-scripts/20-ipython.sh
+++ dockerfiles/hdp_python_node/start-scripts/20-ipython.sh
@@ -4,9 +4,9 @@ source /etc/bashrc
 /etc/init.d/sshd start
 
 # The /root/nltk_data folder gets mapped to the Ubuntu host
-mkdir /root/nltk_data
+mkdir -p /root/nltk_data
 
 export HOME=/root
 cd /root/notebooks
-#ipython notebook --profile=nbserver > /tmp/ipython.log 2>&1
-IPYTHON_OPTS="notebook --pylab inline --profile nbserver" /usr/lib/spark/bin/pyspark
+ipython notebook --profile=nbserver > /tmp/ipython.log 2>&1
+#IPYTHON_OPTS="notebook --pylab inline --profile nbserver" /usr/lib/spark/bin/pyspark
