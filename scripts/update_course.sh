f8c243de09ebd64ad1e098ead99e6cc64ac21333
Michael Bingham <michael.b.bingham@gmail.com>
2014-12-30 23:43:43 -0800

    Update update_course.sh

--- scripts/update_course.sh
+++ scripts/update_course.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
+#TODO: Improve this script to only clone the repo when changes actually exist since the last update
+
 pwd=$PWD
 cd /root/ds
-git pull origin --dry-run | grep -q -v 'Already up-to-date.' && changed=1
-if [[ changed == 1 ]];
-then
-  old=`readlink /root/ds`
-  cd /root
-  ts=`date +"%Y%m%d-%H%M%S"`
-  git clone https://github.com/HortonworksUniversity/DataScience_Rev2.git ds-$ts
-  rm -f /root/ds
-  ln -s /root/ds-$ts /root/ds
-  cd /root/ds
-  git checkout hdp22-sandbox
-  echo "Course updated. Previous version lab files available in $old."
-else
-  echo "No updates found for course."
-fi
-
+old=`readlink /root/ds`
+cd /root
+ts=`date +"%Y%m%d-%H%M%S"`
+git clone https://github.com/HortonworksUniversity/DataScience_Rev2.git ds-$ts
+rm -f /root/ds
+ln -s /root/ds-$ts /root/ds
+cd /root/ds
+git checkout hdp22-sandbox
+echo "Course updated. Previous version lab files available in $old."
 cd $pwd
